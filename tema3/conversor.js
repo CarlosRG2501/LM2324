af1d6ed244741bfec1e07ebd703f148309c6e8ec
CarloRG2501 <carlosramirez05@iesamachado.org>
2024-01-12 14:23:53 +0100

    Añadidos ejercicios de javascript

---
+++ tema3/conversor.js
@@ -0,0 +1,16 @@
+/*Código javascrip del conversor*/
+
+function convertir(divisa){
+    let valor = document.getElementById("cantidad").value;
+    let resultado = 0;
+    if (divisa=="euros"){
+        //Convierto a euro
+        resultado = valor * 0.91;
+        document.getElementById("resultado").innerHTML = resultado + " €";
+    }
+    else {
+        //Convertir a dolares
+        resultado = valor * 1.09;
+        document.getElementById("resultado").innerHTML = resultado + " $";
+    }
+}
