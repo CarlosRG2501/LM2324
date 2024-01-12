b3877cc436470d9fc3de31280bde65ab2d8ed292
CarloRG2501 <carlosramirez05@iesamachado.org>
2024-01-12 11:00:18 +0100

    Añadido ejercicios javascrip

--- tema3/circulo.js
+++ tema3/circulo.js
@@ -1,29 +1,26 @@
-/*function ocultarRojo(){
-    document.getElementById("circulorojo").style.visibility = "hidden"
-
-}
-function ocultarAzul(){
-    document.getElementById("circuloazul").style.visibility = "hidden"
-
+/* 
+Versión cutre 
+function ocultarRojo(){
+    document.getElementById("rojo").style.visibility = "hidden";
 }
 function ocultarVerde(){
-    document.getElementById("circuloverde").style.visibility = "hidden"
-
-}*/
-function ocultar(caja){
-    document.getElementById(caja).style.visibility = "hidden"
+    document.getElementById("verde").style.visibility = "hidden";
 }
-
+function ocultarAzul(){
+    document.getElementById("azul").style.visibility = "hidden";
+}
+*/
+/* Versión mejorada */
+function ocultar(x){
+    document.getElementById(x).style.visibility = "hidden";
+}
 function mostrar(caja){
-    document.getElementById(caja).style.visibility = "visible"
-    
+    document.getElementById(caja).style.visibility = "visible";
 }
-
-/* Version con un solo boton*/
-
-function ocultar_o_mostrar(caja) {
-    if (document.getElementById(caja).style.visibility =="visible")
-        document.getElementById(caja).style.visibility ="hidden"
+/* Versión con un solo boton */
+function ocultar_o_mostrar(caja){
+    if (document.getElementById(caja).style.visibility =="visible" || document.getElementById(caja).style.visibility =="")
+        document.getElementById(caja).style.visibility = "hidden";
     else
-    document.getElementById(caja).style.visibility =="visible"
+        document.getElementById(caja).style.visibility = "visible";
 }
