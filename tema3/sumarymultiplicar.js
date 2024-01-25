6a11fdd71f8a4fe7d68f20795f62a0c1ec0072bf
CarlosRG2501 <carlosramirez05@iesamachado.org>
2024-01-26 00:18:03 +0100

    Añadido ejercicio de sumar y multiplicar

--- tema3/sumarymultiplicar.js
+++ tema3/sumarymultiplicar.js
@@ -1,1 +1,33 @@
-var numeros = ["1", "2", "3"];
+var numeros = [];
+document.getElementById("demo").innerHTML = numeros;
+
+function añadir(){
+
+    const introducir = document.getElementById("introducir");
+    const numero = parseFloat(introducir.value);
+    if(!isNaN(numero)){
+        numeros.push(numero);
+        introducir.value = "";
+    }
+
+    else{
+        alert("Por favor, introduce un valor válido");
+    }
+}
+
+
+function sumar() {
+    const resultado = document.getElementById("resultado");
+    const suma = numeros.reduce();
+    resultado.textContent = 'La suma de los números es: suma';
+}
+
+  
+function multiplicar() {
+    const resultado = document.getElementById("resultado");
+    const multiplicacion = numeros.reduce();
+    resultado.textContent = 'El producto de los números es: multiplicacion';
+   
+ }
+
+
