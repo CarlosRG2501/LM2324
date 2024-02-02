2b267963a2cd089fee7a75155da15eebcebda798
CarloRG2501 <carlosramirez05@iesamachado.org>
2024-02-02 14:28:54 +0100

    añadidas cosas a validar campòs

--- tema3/MachacandoArrays/validacampos.js
+++ tema3/MachacandoArrays/validacampos.js
@@ -1,18 +1,26 @@
 function validar(elementos){
     let estanCorrectos = true;
     for (var i=0;i<elementos.length;i++){
-        document.getElementById("campo"+(i+1).toString()).innerHTML = "";        
-        if (elementos[i].value == "" || (i==4 && !elementos[i].checked)){
+        document.getElementById("campo"+(i+1).toString()).innerHTML = ""; //Por defecto el cammpo está OK 
+        if (elementos[i].value == "" || (i==6 && !elementos[i].checked)){ //Si el campo esta vacío (el campo de verificaciín y no está marcada)
             document.getElementById("campo"+(i+1).toString()).innerHTML = "El campo " + elementos[i].id + " está vacío";
             estanCorrectos = false;
         }
         
     }
-    if (!validarEmail()){
+    if (!validarEmail()){ //Valida el correo
         document.getElementById("campo3").innerHTML = "Email no válido";        
         estanCorrectos = false;
     }
+    if (!validaPasswords()){ //Valida lacontraseña
+        document.getElementById("campo4").innerHTML = "La contraseña no cumple con los requisitos";
+        document.getElementById("campo5").innerHTML = "Las contraseñas no cumplen con los requisitos";           
+        estanCorrectos = false;
+    }
+    if (!validarDNI())
     return estanCorrectos;
+
+    
 }
 /* Función validarEmail tomada de:
 * https://www.coderbox.net/blog/validar-email-usando-javascript-y-expresiones-regulares/
@@ -28,3 +36,26 @@ function validarEmail(){
 	}
     return valido;
 } 
+
+function validaPasswords(){
+    let clave1 = document.getElementById("password1").value;
+    let clave2 = document.getElementById("password2").value;
+    let passwordsOK = true //Cumple con los requisitos establecidos 
+    if(clave1.length<8 || (clave1!=clave2))
+        passwordsOK = false;
+
+    return passwordsOK;
+}
+
+function validarDNI(){
+    let letra= ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E']
+    var cadena = document.getElementById("dni").value; //DNI completo
+    var numero = parseInt(cadena.substring(0,8)); //Parte numerica
+    var letraUsuario = cadena[8]; //Letra "real" del DNI calculada según "formula"
+    var letraReal = letra[numero%23];
+    var dniValido = true;
+    if (letraUsuario!=letraReal)
+        dniValido = false;
+    return dniValido;
+
+}
