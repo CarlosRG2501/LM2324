eedbdc1db51c9c28d5e8460653bb23f25982220a
CarloRG2501 <carlosramirez05@iesamachado.org>
2024-02-09 14:11:28 +0100

    .

--- tema3/MachacandoArrays/mayor.js
+++ tema3/MachacandoArrays/mayor.js
@@ -1,8 +1,23 @@
 function mayor(vector){
-    let mayor=vector[0]; // por defecto el primer elemetno es el mayor
+    let mayor = vector[0]; //por defecto el primer elemento es el mayor
     for (var i=0;i<vector.length;i++){
-    if (vector[i]>mayor);
-    mayor = vector[i];
+        if (vector[i]>mayor)
+        mayor = vector[i]
     }
     return mayor;
+}
+function menor(vector){
+    let menor = vector[0]; //por defecto el primer elemento es el menor
+    for (var i=0;i<vector.length;i++){
+        if (vector[i]<menor)
+        menor = vector[i]
+    }
+    return menor;
+}
+function imprimir(vector){
+    document.getElementById("resultado").innerHTML = ""
+    for( var contador=0;contador<vector.length;contador++){
+        document.getElementById("resultado").innerHTML += "Elemento numero" + contador + ":" 
+        document.getElementById("resultado").innerHTML = document.getElementById("resultado").innerHTML + vector[contador] + "</br>"
+    }
 }
